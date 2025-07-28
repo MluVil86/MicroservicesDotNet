d60057ca3a488a96553918328be771b02ba8c181
Mlungisi Vilakazi <mvilakazi@ers.org.sz>
2025-07-28 15:36:29 +0200

    modify connection string for postgres database, modify dbcontext to get connection string values from environmental variables.  update yaml to include postgres and user container

--- eCommerceSolution.UserService/eCommerce.Infrastructure/DbContext/DapperDbContext.cs
+++ eCommerceSolution.UserService/eCommerce.Infrastructure/DbContext/DapperDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Npgsql;
 using System.Data;
 
@@ -12,8 +13,14 @@ public class DapperDbContext
     public DapperDbContext(IConfiguration configuration)
     {
         _configuration = configuration;
-        string? connectionString = _configuration["ConnectionStrings:PostgresConnection"];
-
+        string? connectionStringTemplate = _configuration.GetConnectionString("PostgresConnection")!;
+        string connectionString = connectionStringTemplate
+                                  .Replace("$POSTGRES_HOST", Environment.GetEnvironmentVariable("POSTGRES_HOST"))
+                                  .Replace("$POSTGRES_PORT", Environment.GetEnvironmentVariable("POSTGRES_PORT"))
+                                  .Replace("$POSTGRES_DATABASE", Environment.GetEnvironmentVariable("POSTGRES_DATABASE"))
+                                  .Replace("$POSTGRES_USERNAME", Environment.GetEnvironmentVariable("POSTGRES_USERNAME"))
+                                  .Replace("$POSTGRES_PASSWORD", Environment.GetEnvironmentVariable("POSTGRES_PASSWORD"));
+        
         //Create a new NpgsqlConnection with the retrieved connection string
         _connection = new NpgsqlConnection(connectionString);   
     }
