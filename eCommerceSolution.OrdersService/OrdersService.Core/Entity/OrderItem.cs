09d0fe45f7c417dc644bd3f700c7c7759d5d3618
Mlungisi Vilakazi <mvilakazi@ers.org.sz>
2025-07-29 17:03:06 +0200

    add orderitem and set data annotations

--- eCommerceSolution.OrdersService/OrdersService.Core/Entity/OrderItem.cs
+++ eCommerceSolution.OrdersService/OrdersService.Core/Entity/OrderItem.cs
@@ -11,8 +11,17 @@ public class OrderItem
 {
     [BsonId]
     [BsonRepresentation(MongoDB.Bson.BsonType.String)]
+    public Guid _id { get; set; }
+
+    [BsonRepresentation(MongoDB.Bson.BsonType.String)]
     public Guid ProductID { get; set; }
+
+    [BsonRepresentation(MongoDB.Bson.BsonType.Decimal128)]
     public decimal UnitPrice { get; set; }
+
+    [BsonRepresentation(MongoDB.Bson.BsonType.Int64)]
     public int Quantity { get; set; }
+
+    [BsonRepresentation(MongoDB.Bson.BsonType.Double)]
     public decimal TotalPrice { get; set; }
 }
